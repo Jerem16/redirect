47b6f95ae44cd8af01bdc4af808c0af06f52b437
Jerem16 <jeremy.lemaignent@gmail.com>
2025-03-06 13:46:48 +0100

    v2

--- app/layout.jsx
+++
@@ -1,8 +0,0 @@
-export default function RootLayout() {
-    return (
-        <html lang="fr-FR">
-            <head></head>
-            <body></body>
-        </html>
-    );
-}
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     experimental: {},
-    reactStrictMode: true,
+
     typescript: {
         // !! WARN !!
         // Dangerously allow production builds to successfully complete even if
