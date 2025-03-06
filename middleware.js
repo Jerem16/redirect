f09f7f7bf3c57bb8a11102cb1391dd27b6644795
Jerem16 <jeremy.lemaignent@gmail.com>
2025-03-06 18:19:14 +0100

    init

--- middleware.js
+++ middleware.js
@@ -1,22 +1,22 @@
 import { NextResponse } from "next/server";
 
 export function middleware(req) {
-    const url = req.nextUrl; // Utilisation correcte de nextUrl
+    const url = req.nextUrl; 
     const userAgent = req.headers.get("user-agent") || "";
     const isMobile = /Mobi|Android/i.test(userAgent);
 
     if (isMobile) {
-        // return NextResponse.redirect(
-        //     new URL(
-        //         "https://mobile-peur-de-la-conduite.wii-design.com",
-        //         req.url
-        //     )
-        // );
+        return NextResponse.redirect(
+            new URL(
+                "https://mobile-peur-de-la-conduite.wii-design.com",
+                req.url
+            )
+        );
     }
 
-    // return NextResponse.redirect(
-    //     new URL("https://desktop-peur-de-la-conduite.wii-design.com", req.url)
-    // );
+    return NextResponse.redirect(
+        new URL("https://desktop-peur-de-la-conduite.wii-design.com", req.url)
+    );
 }
 
 export const config = {
