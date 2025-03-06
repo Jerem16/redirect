40a2a2c99c278526eeb78997487992fdd84f0465
Jerem16 <jeremy.lemaignent@gmail.com>
2025-03-06 03:11:25 +0100

    v2

---
+++ middleware.js
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+    const url = req.nextUrl; // Utilisation correcte de nextUrl
+    const userAgent = req.headers.get("user-agent") || "";
+    const isMobile = /Mobi|Android/i.test(userAgent);
+
+    if (isMobile) {
+        return NextResponse.redirect(
+            new URL(
+                "https://mobile-peur-de-la-conduite.wii-design.com",
+                req.url
+            )
+        );
+    }
+
+    return NextResponse.redirect(
+        new URL("https://desktop-peur-de-la-conduite.wii-design.com", req.url)
+    );
+}
+
+export const config = {
+    matcher: "/", // Applique ce middleware uniquement à la racine "/"
+};
